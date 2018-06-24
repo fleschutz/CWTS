e892d84304b824dbc47438132a2e717c2804e682
Markus Fleschutz <markus@fleschutz.de>
2018-06-24 09:18:24 +0200

    Added shift_count_negative.cpp and shift_count_overflow.cpp

---
+++ Tests/shift_count_negative.cpp
@@ -0,0 +1,6 @@
+extern int shift_count_negative(int a);
+
+int shift_count_negative(int a)
+{
+	return a << -1;
+}
---
+++ Tests/shift_count_overflow.cpp
@@ -0,0 +1,6 @@
+extern int shift_count_overflow(int a);
+
+int shift_count_overflow(int a)
+{
+	return a << 66;
+}
