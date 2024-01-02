abe6070dcbb17c7bf63df3f746625407de744660
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-01-02 13:38:02 +0100

    Moved the comment in parentheses.cpp

--- tests/parentheses.cpp
+++ tests/parentheses.cpp
@@ -2,10 +2,10 @@ extern int parentheses(int value);
 
 int parentheses(int value)
 {
-	if (value > 0)
+	if (value > 0) // oops, explicit parentheses recommended
 		if (value == 7)
 			return 1;
-	else // oops, parentheses recommended
+	else
 		return 2;
 	return 3;
 }
