daa64b6b7388a4a2613962f069b4888d395aa3ff
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-01-10 12:42:09 +0100

    Improved CMakeLists.txt and assignment_in_condition_1.cpp

--- tests/assignment_in_condition_1.cpp
+++ tests/assignment_in_condition_1.cpp
@@ -2,7 +2,7 @@ extern bool assignment_in_condition_1(int alpha);
 
 bool assignment_in_condition_1(int alpha)
 {
-	if (alpha = 5)   // oops, 'alpha = 5' will evaluate to value 5, which is always true
+	if (alpha = 5)   // ⚠️ 'alpha = 5' will evaluate to value 5, which is always true
 		return true;
 	return false;
 }
